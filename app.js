f894eec4cb5b11442d02d6136bd35e6c90ee794c
Jaeyoon <sbjeong222@gmail.com>
2022-06-22 14:50:19 +0900

    바닐라 JS 기초(5)

--- app.js
+++ app.js
@@ -1,59 +1,13 @@
-//-------JS 파일에서 HTML element로 접근하기-------
-const h1 = document.getElementById("title");
+const h1 = document.querySelector("#title");
 
-//여러가지 접근 방식의 코드
-// const h1 = document.getElementsByClassName("h1");
-// const h1 = document.querySelector(".h1");
-
-h1.innerText = "Let's check!";
-console.log(h1);
-console.dir(h1);
-
-//Function list
+//classname 을 추가하고 삭제하며 css파일을 통해 style 넣기
 function handleTitleClick() {
-    h1.innerText = "mouse is clicked";
-    //Code 1.
-    // if (h1.style.color === "black") {
-    //     h1.style.color = "blue";
-    // } else {
-    //     h1.style.color = "black";
-    // }
-
-    //Code 1.1
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "black") {
-        newColor = "blue";
+    const clickedClass = "clicked"; 
+    if (h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "black";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
-}
-function mouseIn() {
-    h1.innerText = "mouse is here!";
-}
-function mouseOut() {
-    h1.innerText = " mouse is gone...";
 }
 
-//Event 추가
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", mouseIn);
-h1.addEventListener("mouseleave", mouseOut);
-
-//Event 추가 (다른 코드)
-// h1.onclick = handleTitleClick;
-
-//-------JS 에서 Window object 접근하기-------
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-window.addEventListener("resize", handleWindowResize);
-
-
-
-//-------요약-------
-//step 1. element 찾기
-//step 2. event 를 listen 하기
-//step 3. event 에 반응하기
