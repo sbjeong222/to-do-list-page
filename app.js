9620c98a073263c92bd27e621b6372bc94a08b7b
Jaeyoon <sbjeong222@gmail.com>
2022-06-22 15:21:54 +0900

    바닐라 JS 기초(6)

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const h1 = document.querySelector("#title");
 
 //classname 을 추가하고 삭제하며 css파일을 통해 style 넣기
 function handleTitleClick() {
-    const clickedClass = "clicked"; 
-    if (h1.className === clickedClass) {
-        h1.className = "";
-    } else {
-        h1.className = clickedClass;
-    }
+    //-------Code 1.-------
+    // const clickedClass = "clicked";
+    // if (h1.className === clickedClass) {
+    //     h1.className = "";
+    // } else {
+    //     h1.className = clickedClass;
+    // }
+
+    //-------Code 1.1-------
+    // const clickedClass = "clicked";
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
+
+//-------Code 1.2-------
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
